44f67666dab099bd493d89a92e4fbd92c8af6baf
marcs-feh <marcosfehlauer@protonmail.com>
2025-01-18 23:44:48 -0300

    ohh yeah

--- build_ft.sh
+++ build_ft.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
-mkdir -p ./FreeType
+set -eu
 
-mason setup --wipe ft_build \
+mkdir -p ./ft_install
+mkdir -p ./ft_build
+
+meson setup ft_build \
 	--default-library static \
-	--prefix "$(pwd)/FreeType" \
+	--prefix "$(pwd)/ft_install" \
 	-Dharfbuzz=disabled \
 	-Dbrotli=disabled \
 	-Dbzip2=disabled \
@@ -13,5 +16,18 @@ mason setup --wipe ft_build \
 	-Dtests=disabled \
 	-Dzlib=internal
 
-ninja -j$(($(nproc) * 2)) -C ft_build install
+ninja -v -j$(($(nproc) * 2)) -C ft_build install
+
+mkdir ft_out
+cp docs/FTL.TXT ft_out/LICENSE
+cp -r ft_install/include/freetype2 ft_out
+
+for lib in $(find ft_install -name '*.a'); do
+	cp "$lib" ft_out
+done
+
+rm -f /build/freetype_slim.zip
+mv ft_out freetype_slim
+zip -r -9 freetype_slim.zip freetype_slim
+mv freetype_slim.zip /build
 
