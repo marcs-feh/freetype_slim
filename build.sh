8ee5bf7705379fa840c75e0e14797c367cebdd62
marcs-feh <marcosfehlauer@protonmail.com>
2025-06-17 15:29:48 -0300

    Update build

--- build.sh
+++ build.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env sh
+
+build_mode="$1"
+
 set -eu
 
-# musl (nice)
-podman build -f Containerfile_linux_musl -t freetype-slim-linux-musl
-podman run --replace --name freetype_slim_builder -v $(pwd):/build freetype-slim-linux-musl:latest
-podman rm freetype_slim_builder
-podman unshare rm -rf freetype
+Help(){
+	echo "$0: [musl|glibc]"
+}
 
-# glibc (eww)
-# podman build -f Containerfile_linux_glibc -t freetype-slim-linux-glibc
-# podman run --replace --name freetype_slim_builder -v $(pwd):/build freetype-slim-linux-glibc:latest
-# podman rm freetype_slim_builder
-# podman unshare rm -rf freetype
+BuildMusl(){
+	podman build -f Containerfile_linux_musl -t freetype-slim-linux-musl
+	podman run --replace --name freetype_slim_builder -v $(pwd):/build freetype-slim-linux-musl:latest
+	podman rm freetype_slim_builder
+	podman unshare rm -rf freetype
+}
 
+BuildGlibc(){
+	podman build -f Containerfile_linux_glibc -t freetype-slim-linux-glibc
+	podman run --replace --name freetype_slim_builder -v $(pwd):/build freetype-slim-linux-glibc:latest
+	podman rm freetype_slim_builder
+	podman unshare rm -rf freetype
+}
+
+case "$build_mode" in
+	"musl") BuildMusl ;;
+	"glibc") BuildGlibc ;;
+	*) Help && exit 1 ;;
+esac
